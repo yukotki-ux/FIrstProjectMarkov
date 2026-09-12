8d888ee77e7a557a2122bd5c2110c39da90c5038
yukotki-ux <11102218@cc.peralta.edu>
2026-09-12 19:58:04 +0000

    compile

--- markov.h
+++ markov.h
@@ -4,4 +4,15 @@
 
 #endif
 
-std::string joinWords(const std::string words[], int startIndex, int count);
+std::string joinWords(const std::string words[], int startIndex, int count){
+
+std::string result = "";
+
+for (int i = 0; i < count; i++){
+    result += words[startIndex + i];
+    if(i < count - 1){
+        result += " ";
+    }
+}
+return result;
+}
